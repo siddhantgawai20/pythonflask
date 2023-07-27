2954a4d2e14c4e130926ce0542368f4f67d88b05
Siddhant Gawai <siddhantgawai20@gmail.com>
2023-07-27 09:54:22 +0530

    Changes done

--- server.py
+++ server.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
 
 @app.route('/', methods=["GET"])
 def root():
-  return "<h1> WELCOME TO MY APPLICATION (v1)</h1>"
+  return '<h1 style="color: red"> WELCOME TO MY APPLICATION (v2)</h1>'
 
 app.run(host="0.0.0.0", port=4000)
