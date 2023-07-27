ef1bc04e4b8ccdd9bbc0692304f5f33a6d915f69
Siddhant Gawai <siddhantgawai20@gmail.com>
2023-07-27 08:57:17 +0530

    initial commit

---
+++ server.py
@@ -0,0 +1,9 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', methods=["GET"])
+def root():
+  return "<h1> WELCOME TO MY APPLICATION (v1)</h1>"
+
+app.run(host="0.0.0.0", port=4000)
